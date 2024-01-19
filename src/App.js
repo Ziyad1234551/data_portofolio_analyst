912d1b56d439ecf82c8debd9910ed20912da5221
Ziyad <ziya19128@student.nurulfikri.ac.id>
2024-01-19 17:38:27 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
     });
   }, [])
   return (
+    <>
    <div>
     <Navbar/>
     <Hero/>
@@ -25,6 +26,7 @@ function App() {
     <Testimonial/>
     <Footer/>
    </div>
+    </>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 export const Footer = () => {
   return (
+    <>
     <div data-aos="fade-bottom" className='my-[60px]'>
         <h1 className=' text-2xl lg:text-5xl text-center font-bold'>Get In Touch</h1>
         <div className='flex justify-center text-[50px] font-bold gap-10 my-10'>
@@ -19,5 +20,6 @@ export const Footer = () => {
         </div>
 
     </div>
+    </>
   )
 }
